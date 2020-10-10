54012eb68a0080dc82269ec429dfd20af3e83086
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-10 17:37:27 -0400

    Update master_node_config.sh
    Added cgroup driver change for Docker

--- master_node_config.sh
+++ master_node_config.sh
@@ -5,6 +5,11 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"]
+}
+EOF
 sudo apt autoremove
 sudo apt-get update
 sudo apt-get install -y docker-ce kubelet=1.19.2-00 kubeadm=1.19.2-00 kubectl=1.19.2-00 avahi-daemon libnss-mdns
