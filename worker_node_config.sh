dc6fecbbb83d36017600d9530c547fa9528eac3a
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-10 18:00:25 -0400

    Update worker_node_config.sh

--- worker_node_config.sh
+++ worker_node_config.sh
@@ -5,6 +5,9 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
+sudo apt autoremove
+sudo apt-get update
+sudo apt-get install -y docker-ce kubelet=1.19.2-00 kubeadm=1.19.2-00 kubectl=1.19.2-00 avahi-daemon libnss-mdns
 sudo touch /etc/docker/daemon.json
 sudo chmod 755 /etc/docker/daemon.json
 cat > /etc/docker/daemon.json <<EOF
@@ -12,13 +15,10 @@ cat > /etc/docker/daemon.json <<EOF
   "exec-opts": ["native.cgroupdriver=systemd"]
 }
 EOF
-systemctl daemon-reload
-systemctl enable kubelet && systemctl restart kubelet
-systemctl enable docker && systemctl restart docker
-usermod -aG docker vagrant
-sudo apt autoremove
-sudo apt-get update
-sudo apt-get install -y docker-ce kubelet=1.19.2-00 kubeadm=1.19.2-00 kubectl=1.19.2-00 avahi-daemon libnss-mdns
+sudo systemctl daemon-reload
+sudo systemctl enable kubelet && systemctl restart kubelet
+sudo systemctl enable docker && systemctl restart docker
+sudo usermod -aG docker vagrant
 sudo apt update
 sudo apt upgrade -y
 sudo reboot
