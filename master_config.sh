25758d59438419491d8c3efde227bfdaef7d0d55
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-11 13:57:34 -0400

    Update master_config.sh

--- master_config.sh
+++ master_config.sh
@@ -1,5 +1,5 @@
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
-sudo mkdir -p $HOME/.kube
+mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
