a1399ba92a700e450642e11abfd67e8fdef8bbb8
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-10 22:41:13 -0400

    Update and rename master_node_config.sh to master_config.sh

---
+++ master_config.sh
@@ -0,0 +1,8 @@
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+sudo mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+sudo chmod 700 get_helm.sh
+sudo ./get_helm.sh
