76a9bdd5ada08ea09d7c338fbadf8a755eb727e3
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-11 21:13:17 -0400

    Update master_config.sh

--- master_config.sh
+++ master_config.sh
@@ -3,7 +3,7 @@ sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 mkdir -p "/home/vagrant/.kube"
 sudo cp -i /etc/kubernetes/admin.conf "/home/vagrant/.kube/config"
 sudo chown vagrant:vagrant "/home/vagrant/.kube/config"
-read -t 5 -p "Waiting 5 seconds for K8S services to stabilize before continuing  ..."
+sleep 1m
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 sudo chmod 700 get_helm.sh
