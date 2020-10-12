726f06e9827c8418914212ae5e172f2908c7dce1
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-11 21:46:13 -0400

    Update common_node_config.sh

--- common_node_config.sh
+++ common_node_config.sh
@@ -11,9 +11,9 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-sudo apt-get update
+sudo apt-get update --quiet
 # Install Docker, K8S & mDNS Responder
-sudo apt-get install -y docker-ce kubelet=$K8S_VERSION kubeadm=$K8S_VERSION kubectl=$K8S_VERSION avahi-daemon libnss-mdns unattended-upgrades
+sudo apt-get install -y docker-ce kubelet=$K8S_VERSION kubeadm=$K8S_VERSION kubectl=$K8S_VERSION avahi-daemon libnss-mdns unattended-upgrades --quiet
 # Docker Configuration
 sudo touch /etc/docker/daemon.json && sudo chmod 500 /etc/docker/daemon.json
 cat > /etc/docker/daemon.json <<EOF
