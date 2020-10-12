757c4e7fef460a5da68dbc4740f84bb9bf3d005d
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-12 09:20:43 -0400

    Update master_config.sh

--- master_config.sh
+++ master_config.sh
@@ -1,11 +1,15 @@
+# Initialize K8S cluster
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 mkdir -p "/home/vagrant/.kube"
 sudo cp -i "/etc/kubernetes/admin.conf" "/home/vagrant/.kube/config"
 sudo chown vagrant:vagrant "/home/vagrant/.kube/config"
+# Install WeaveNet CNI
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-# Download & install Helm Package Manager for K8S
+# Install Helm Package Manager for K8S
 sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 sudo chmod 700 get_helm.sh
 sudo ./get_helm.sh
 helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+# Cleanup
 sudo rm -f get_helm.sh
+sudo rm -f master_config.sh
