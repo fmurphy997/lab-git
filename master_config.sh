817280b6622ea7762e86cf0f03f196d90a84ee93
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-12 08:49:25 -0400

    Update master_config.sh

--- master_config.sh
+++ master_config.sh
@@ -2,9 +2,8 @@ sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 mkdir -p "/home/vagrant/.kube"
 sudo cp -i "/etc/kubernetes/admin.conf" "/home/vagrant/.kube/config"
 sudo chown vagrant:vagrant "/home/vagrant/.kube/config"
-wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-sudo chown vagrant:vagrant "/home/vagrant/kube-flannel.yml"
-sudo -u vagrant kubectl apply -f kube-flannel.yml
+sudo -u vagrant kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+# Download & install Helm Package Manager for K8S
 sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 sudo chmod 700 get_helm.sh
 sudo ./get_helm.sh
