319c732580bbbdf42b2485aeaf4c5e1f236ab8f0
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-11 23:23:56 -0400

    Update master_config.sh

--- master_config.sh
+++ master_config.sh
@@ -2,7 +2,8 @@ sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 mkdir -p "/home/vagrant/.kube"
 sudo cp -i "/etc/kubernetes/admin.conf" "/home/vagrant/.kube/config"
 sudo chown vagrant:vagrant "/home/vagrant/.kube/config"
-kubectl apply -f https://docs.projectcalico.org/manifests/calico-etcd.yaml
+wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl apply kube-flannel.yml
 sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 sudo chmod 700 get_helm.sh
 sudo ./get_helm.sh
