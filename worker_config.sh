fc31db4685138b40430b24dc666aa9c75877c8e0
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-12 18:22:40 -0400

    Update worker_config.sh

--- worker_config.sh
+++ worker_config.sh
@@ -1,3 +1,4 @@
 set -eo pipefail
 discovery_token_ca_cert_hash="$(grep 'discovery-token-ca-cert-hash' /vagrant/kubeadm.log | head -n1 | awk '{print $2}')"
 sudo kubeadm join #{CONTROLLER_IP}:#{MASTER_PORT} --token #{JOIN_TOKEN} --discovery-token-ca-cert-hash ${discovery_token_ca_cert_hash}
+sudo rm -f /vagrant/kubeadm.log
