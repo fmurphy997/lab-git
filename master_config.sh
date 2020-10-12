4f841f82f4d560d1ceea79e2d183f6726b4128c1
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-11 20:27:32 -0400

    Update master_config.sh

--- master_config.sh
+++ master_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 mkdir -p "/home/vagrant/.kube"
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
 sudo chown $(id -u):$(id -g) /home/vagrant/.kube/config
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
