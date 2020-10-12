73846646064c05ab4f81513601f6b1381cd07f97
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-12 08:32:55 -0400

    Update master_config.sh

--- master_config.sh
+++ master_config.sh
@@ -4,7 +4,7 @@ sudo cp -i "/etc/kubernetes/admin.conf" "/home/vagrant/.kube/config"
 sudo chown vagrant:vagrant "/home/vagrant/.kube/config"
 wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 sudo chown vagrant:vagrant "/home/vagrant/kube-flannel.yml"
-sudo kubectl apply -f kube-flannel.yml
+sudo -u vagrant kubectl apply -f kube-flannel.yml
 sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 sudo chmod 700 get_helm.sh
 sudo ./get_helm.sh
