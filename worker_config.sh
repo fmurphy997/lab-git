a2f72bb482850f88ee278206c94fee0cdb30c703
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-12 21:24:15 -0400

    Update worker_config.sh

--- worker_config.sh
+++ worker_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./deployment_variables.sh
+/bin/su -c "/home/vagrant/deployment_variables.sh" - vagrant
 
 set -eo pipefail
 discovery_token_ca_cert_hash="$(grep 'discovery-token-ca-cert-hash' /vagrant/kubeadm.log | head -n1 | awk '{print $2}')"
