e36192fc845b26132e359ad3aed565d705136cf2
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-13 18:27:25 -0400

    Update worker_config.sh

--- worker_config.sh
+++ worker_config.sh
@@ -5,3 +5,6 @@ source ./deployment_variables.conf
 set -eo pipefail
 discovery_token_ca_cert_hash="$(grep 'discovery-token-ca-cert-hash' /vagrant/kubeadm.log | head -n1 | awk '{print $2}')"
 sudo kubeadm join ${CONTROLLER_IP}:${CONTROLLER_PORT} --token ${JOIN_TOKEN} --discovery-token-ca-cert-hash ${discovery_token_ca_cert_hash}
+sudo sed -i 's/.$//' /var/lib/kubelet/kubeadm-flags.env
+sudo sed -i "s/$/ --node-ip=$KUBELET_IP/" "/var/lib/kubelet/kubeadm-flags.env"
+sudo sed -i 's/$/"/' /var/lib/kubelet/kubeadm-flags.env
