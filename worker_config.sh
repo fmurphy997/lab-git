2517e17b4c22c3a634a677035d9e676ca9923add
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-12 20:27:23 -0400

    Update worker_config.sh

--- worker_config.sh
+++ worker_config.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-JOIN_TOKEN=ayngk7.m1555duk5x2i3ctt
-CONTROLLER_IP=172.16.30.100
-CONTROLLER_PORT=8443
 
 set -eo pipefail
 discovery_token_ca_cert_hash="$(grep 'discovery-token-ca-cert-hash' /vagrant/kubeadm.log | head -n1 | awk '{print $2}')"
