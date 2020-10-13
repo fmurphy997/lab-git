f3a247d146384ff2a3f35317a2cd626bbfb71aa8
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-12 21:13:43 -0400

    Update deployment_variables.sh

--- deployment_variables.sh
+++ deployment_variables.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
-export DISTRO_ID=Ubuntu
-export DISTRO_CODENAME=bionic
-export JOIN_TOKEN=ayngk7.m1555duk5x2i3ctt
-export CONTROLLER_IP=172.16.30.100
-export CONTROLLER_PORT=8443
-export K8S_VERSION=1.19.2
-export K8S_APT_VERSION=1.19.2-00
-export POD_NW_CIDR=10.244.0.0/16
-export IMAGE_REPO=k8s.gcr.io
+export "DISTRO_ID=Ubuntu"
+export "DISTRO_CODENAME=bionic"
+export "JOIN_TOKEN=ayngk7.m1555duk5x2i3ctt"
+export "CONTROLLER_IP=172.16.30.100"
+export "CONTROLLER_PORT=8443"
+export "K8S_VERSION=1.19.2"
+export "K8S_APT_VERSION=1.19.2-00"
+export "POD_NW_CIDR=10.244.0.0/16"
+export "IMAGE_REPO=k8s.gcr.io"
