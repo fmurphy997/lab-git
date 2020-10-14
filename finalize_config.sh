bd22389e2294da07261f5086b0e625eaee752d2e
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-13 20:23:25 -0400

    Update and rename kube_apps to finalize_config.sh

---
+++ finalize_config.sh
@@ -0,0 +1,5 @@
+sudo sed -i 's/.$//' /var/lib/kubelet/kubeadm-flags.env
+sudo sed -i "s/$/ --node-ip=$KUBELET_IP/" "/var/lib/kubelet/kubeadm-flags.env"
+sudo sed -i 's/$/"/' /var/lib/kubelet/kubeadm-flags.env
+sudo systemctl daemon-reload
+sudo systemctl restart kubelet
