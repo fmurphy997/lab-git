638fff5e347396c862586a8ed4f2723506f3d71d
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-14 11:50:28 -0400

    Delete finalize_config.sh

--- finalize_config.sh
+++
@@ -1,6 +0,0 @@
-# Set K8S Internal-IP on all nodes
-sudo sed -i 's/.$//' /var/lib/kubelet/kubeadm-flags.env
-sudo sed -i "s/$/ --node-ip=$KUBELET_IP/" "/var/lib/kubelet/kubeadm-flags.env"
-sudo sed -i 's/$/"/' /var/lib/kubelet/kubeadm-flags.env
-sudo systemctl daemon-reload
-sudo systemctl restart kubelet
