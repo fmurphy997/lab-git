d236f3724a31fb58b4aa3f7aa3be645a73f789a8
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-13 20:36:24 -0400

    Update worker_config.sh

--- worker_config.sh
+++ worker_config.sh
@@ -3,5 +3,13 @@
 source ./deployment_variables.conf
 
 set -eo pipefail
+
+# Join node to K8S cluster
 discovery_token_ca_cert_hash="$(grep 'discovery-token-ca-cert-hash' /vagrant/kubeadm.log | head -n1 | awk '{print $2}')"
 sudo kubeadm join ${CONTROLLER_IP}:${CONTROLLER_PORT} --token ${JOIN_TOKEN} --discovery-token-ca-cert-hash ${discovery_token_ca_cert_hash}
+# Change K8S Internal-IP to non-NAT interface
+sudo sed -i 's/.$//' /var/lib/kubelet/kubeadm-flags.env
+sudo sed -i "s/$/ --node-ip=$KUBELET_IP/" "/var/lib/kubelet/kubeadm-flags.env"
+sudo sed -i 's/$/"/' /var/lib/kubelet/kubeadm-flags.env
+sudo systemctl daemon-reload
+sudo systemctl restart kubelet
