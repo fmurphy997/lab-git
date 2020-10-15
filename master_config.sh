76c4bcd2eb4c7368cf7a13ae1ca463237b7911a5
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-15 10:22:31 -0400

    Update master_config.sh

--- master_config.sh
+++ master_config.sh
@@ -25,5 +25,6 @@ sudo systemctl restart kubelet
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install consul
+export CONSUL_HTTP_ADDR="http://localhost:30101"
 # Quick Cleanup
 sudo rm -f master_config.sh
