4d79295f7a643e175256d4e802609feeeb261850
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-15 08:40:25 -0400

    Update master_config.sh

--- master_config.sh
+++ master_config.sh
@@ -21,13 +21,5 @@ sudo sed -i "s/$/ --node-ip=$KUBELET_IP/" "/var/lib/kubelet/kubeadm-flags.env"
 sudo sed -i 's/$/"/' /var/lib/kubelet/kubeadm-flags.env
 sudo systemctl daemon-reload
 sudo systemctl restart kubelet
-# Install Helm Package Manager for K8S
-sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-sudo chmod 700 get_helm.sh
-sudo ./get_helm.sh
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
-helm repo add hashicorp https://helm.releases.hashicorp.com
-helm repo update
 # Quick Cleanup
-sudo rm -f get_helm.sh
 sudo rm -f master_config.sh
