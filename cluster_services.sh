c33d76e6fb9d659c0e45df77a0e2a614834834cc
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-15 17:55:04 -0400

    Update cluster_services.sh

--- cluster_services.sh
+++ cluster_services.sh
@@ -2,7 +2,7 @@
 sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 sudo chmod 700 get_helm.sh
 sudo ./get_helm.sh
-# Add & refresh repositories
+# Add & refresh Helm repositories
 helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 helm repo add hashicorp https://helm.releases.hashicorp.com
 helm repo add gitlab https://charts.gitlab.io/
