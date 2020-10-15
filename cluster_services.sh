3fec8bcb3956f97ac8f1dced2079a586e120cbbe
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-15 18:02:22 -0400

    Update cluster_services.sh

--- cluster_services.sh
+++ cluster_services.sh
@@ -7,3 +7,12 @@ helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 helm repo add hashicorp https://helm.releases.hashicorp.com
 helm repo add gitlab https://charts.gitlab.io/
 helm repo update
+# Install HashiCorp Consul
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install consul
+# Allow management of Consul from local console
+export CONSUL_HTTP_ADDR="http://localhost:30101"
+# Generate Gossip encryption keyand create K8S Gossip Secret
+export GOSSIP_KEY=$(consul keygen)
+kubectl generate secret generic consul-gossip-encryption-key --from-literal=$GOSSIP_KEY
