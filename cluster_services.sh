302b2d3b1c7a73253f9fe8241af5902c36776b29
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-15 10:12:05 -0400

    Update cluster_services.sh

--- cluster_services.sh
+++ cluster_services.sh
@@ -1,1 +1,8 @@
-
+# Install Helm Package Manager for K8S
+sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+sudo chmod 700 get_helm.sh
+sudo ./get_helm.sh
+# Add & refresh repositories
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm repo add hashicorp https://helm.releases.hashicorp.com
+helm repo update
