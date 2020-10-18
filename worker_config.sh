c6c69f157d5880e2b7a3d8c9d204013a2945f558
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-18 13:38:04 -0400

    Update worker_config.sh

--- worker_config.sh
+++ worker_config.sh
@@ -4,6 +4,9 @@ source ./deployment_variables.conf
 
 set -eo pipefail
 
+#let Controller Node complete its setup
+sleep 90
+
 # Join node to K8S cluster
 discovery_token_ca_cert_hash="$(grep 'discovery-token-ca-cert-hash' /vagrant/kubeadm.log | head -n1 | awk '{print $2}')"
 sudo kubeadm join ${CONTROLLER_IP}:${CONTROLLER_PORT} --token ${JOIN_TOKEN} --discovery-token-ca-cert-hash ${discovery_token_ca_cert_hash}
