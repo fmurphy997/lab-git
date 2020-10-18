8c83cbbdc642b9ad47e303e4c10940c6577aae1b
fmurphy997 <55931459+fmurphy997@users.noreply.github.com>
2020-10-18 13:19:08 -0400

    Update master_config.sh

--- master_config.sh
+++ master_config.sh
@@ -22,9 +22,9 @@ sudo sed -i 's/$/"/' /var/lib/kubelet/kubeadm-flags.env
 sudo systemctl daemon-reload
 sudo systemctl restart kubelet
 # Install HashiCorp Consul
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install consul
-export CONSUL_HTTP_ADDR="http://localhost:30101"
+#curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+#sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+#sudo apt-get update && sudo apt-get install consul
+#export CONSUL_HTTP_ADDR="http://localhost:30101"
 # Quick Cleanup
 sudo rm -f master_config.sh
